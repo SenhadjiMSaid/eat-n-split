2194af56da12da60b8fbbe58acb0d74b89c194e4
SenhadjiMSaid <saidsenhadji06@gmail.com>
2023-12-24 20:53:16 +0100

    Add Key props to FormSplitBill componant

--- src/App.js
+++ src/App.js
@@ -78,6 +78,7 @@ export default function App() {
       </div>
       {selectedFriend && (
         <FormSplitBill
+          key={selectedFriend?.name}
           selectedFriend={selectedFriend}
           onSplitBill={handelSplitBill}
         />
